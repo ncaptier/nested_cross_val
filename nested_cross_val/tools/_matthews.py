1f1b075c763daf30893b8f35448e717773aebef0
ncaptier <captier.nicolas@gmail.com>
2022-10-10 17:53:36 +0200

    Update tools

--- nested_cross_val/tools/_matthews.py
+++ nested_cross_val/tools/_matthews.py
@@ -8,20 +8,21 @@ the user to read the two following articles for more details :
     - https://arxiv.org/abs/2006.11278
 """
 
-def nmcc_from_confusion(tp , fp , fn , tn): 
+
+def nmcc_from_confusion(tp, fp, fn, tn):
     """ Compute the normalized Matthews correlation coefficient from a confusion matrix.
-    
+
     Parameters
     ----------
     tp : int
         Number of true positives
-        
+
     fp : int
         Number of false positives
-    
+
     fn : int
         Number of false negatives
-        
+
     tn : int
         Number of true negatives
 
@@ -29,28 +30,29 @@ def nmcc_from_confusion(tp , fp , fn , tn):
     -------
     Float between 0 and 1
         Normalized Matthews correlation coefficient.
-        
+
     """
-    denom = (tp + fp)*(tp + fn)*(tn + fp)*(tn + fn)
+    denom = (tp + fp) * (tp + fn) * (tn + fp) * (tn + fn)
     if denom <= 0:
         return 0.5
     else:
-        return 0.5*((tp*tn - fp*fn)/np.sqrt(denom) +1)
-        
-def f1_from_confusion(tp , fp , fn , tn):
+        return 0.5 * ((tp * tn - fp * fn) / np.sqrt(denom) + 1)
+
+
+def f1_from_confusion(tp, fp, fn, tn):
     """ Compute the F1 score from a confusion matrix.
-    
+
     Parameters
     ----------
     tp : int
         Number of true positives
-        
+
     fp : int
         Number of false positives
-    
+
     fn : int
         Number of false negatives
-        
+
     tn : int
         Number of true negatives
 
@@ -60,18 +62,19 @@ def f1_from_confusion(tp , fp , fn , tn):
         F1 score.
 
     """
-    f1 = tp/(tp + 0.5*(fp+ fn))
+    f1 = tp / (tp + 0.5 * (fp + fn))
     return f1
 
-def mcc_f1_curve(y_true , y_score):
+
+def mcc_f1_curve(y_true, y_score):
     """ Compute the MCC- F1 curve
-    
+
 
     Parameters
     ----------
     y_true : ndarray of shape (n_samples,) or (n_samples, n_classes)
         True binary labels or binary label indicators.
-        
+
     y_score : ndarray of shape (n_samples,) or (n_samples, n_classes)
         Target scores, can either be probability estimates of the positive
         class, confidence values, or non-thresholded measure of decisions
@@ -81,43 +84,44 @@ def mcc_f1_curve(y_true , y_score):
     -------
     f1s : TYPE
         F1 scores for different thresholds
-        
+
     nmccs : TYPE
         Normalized Matthews correlation coefficients for different thresholds
-        
+
     thresholds : TYPE
         Decreasing thresholds on y_score used to compute F1 score and Matthews correlation coefficient.
-    
+
     Note
     -----
-    
+
     Please refer to https://arxiv.org/abs/2006.11278 for all the details.
-        
+
     """
-    N , P = np.sum(y_true == 0) , np.sum(y_true == 1) 
-    
-    fps , tps , thresholds = _binary_clf_curve(y_true, y_score)
+    N, P = np.sum(y_true == 0), np.sum(y_true == 1)
+
+    fps, tps, thresholds = _binary_clf_curve(y_true, y_score)
     fns = P - tps
     tns = N - fps
-    
+
     nmccs = np.vectorize(nmcc_from_confusion)(tps, fps, fns, tns)
     f1s = f1_from_confusion(tps, fps, fns, tns)
-    
-    return f1s , nmccs , thresholds
-    
-def mcc_f1_score(y_true , y_score , bins = 100):
-    """ Compute the MCC-F1 score associated with the MCC-F1 curve    
+
+    return f1s, nmccs, thresholds
+
+
+def mcc_f1_score(y_true, y_score, bins=100):
+    """ Compute the MCC-F1 score associated with the MCC-F1 curve
 
     Parameters
     ----------
     y_true : ndarray of shape (n_samples,) or (n_samples, n_classes)
         True binary labels or binary label indicators.
-        
+
     y_score : ndarray of shape (n_samples,) or (n_samples, n_classes)
         Target scores, can either be probability estimates of the positive
         class, confidence values, or non-thresholded measure of decisions
         (as returned by :term:`decision_function` on some classifiers).
-        
+
     bins : int, optional
         Number of sub-ranges for the full range of normalized. The default is 100.
 
@@ -125,42 +129,42 @@ def mcc_f1_score(y_true , y_score , bins = 100):
     -------
     metric : float between 0 and 1
         MCC-F1 metric to assess the performance of the classifier.
-        
+
     best_thresholds : float
         Threshold that provides the best prediction performance.
-        
+
     Note
     -----
-    
+
     Please refer to https://arxiv.org/abs/2006.11278 for all the details.
 
     """
-    
-    f1s , nmccs , thresholds = mcc_f1_curve(y_true , y_score)
-    distances = np.sqrt((1 - f1s)**2 + (1 - nmccs)**2
+
+    f1s, nmccs, thresholds = mcc_f1_curve(y_true, y_score)
+    distances = np.sqrt((1 - f1s) ** 2 + (1 - nmccs) ** 2
                         )
     idmax = np.argmax(nmccs)
-    left_nmccs , left_d = nmccs[:idmax+1] , distances[: idmax + 1]
-    right_nmccs , right_d = nmccs[idmax+1 :] , distances[idmax + 1 :]
-    
-    minimum , maximum = np.min(nmccs) , np.max(nmccs)
-    w = (maximum - minimum)/bins
-    
+    left_nmccs, left_d = nmccs[:idmax + 1], distances[: idmax + 1]
+    right_nmccs, right_d = nmccs[idmax + 1:], distances[idmax + 1:]
+
+    minimum, maximum = np.min(nmccs), np.max(nmccs)
+    w = (maximum - minimum) / bins
+
     # Initialisation
-    idx_L = np.argwhere((left_nmccs >= minimum)*(left_nmccs <= minimum + w))
-    idx_R = np.argwhere((right_nmccs >= minimum)*(right_nmccs <= minimum + w))
-    L = [np.mean(left_d[idx_L])] if len(idx_L)> 0 else []
-    R = [np.mean(right_d[idx_R])] if len(idx_R)> 0 else []
-        
+    idx_L = np.argwhere((left_nmccs >= minimum) * (left_nmccs <= minimum + w))
+    idx_R = np.argwhere((right_nmccs >= minimum) * (right_nmccs <= minimum + w))
+    L = [np.mean(left_d[idx_L])] if len(idx_L) > 0 else []
+    R = [np.mean(right_d[idx_R])] if len(idx_R) > 0 else []
+
     for i in range(1, bins):
-        idx_L = np.argwhere((left_nmccs > minimum + i*w)*(left_nmccs <= minimum + (i+1)*w))
-        idx_R = np.argwhere((right_nmccs > minimum + i*w)*(right_nmccs <= minimum + (i+1)*w))        
-        if len(idx_L)> 0:
+        idx_L = np.argwhere((left_nmccs > minimum + i * w) * (left_nmccs <= minimum + (i + 1) * w))
+        idx_R = np.argwhere((right_nmccs > minimum + i * w) * (right_nmccs <= minimum + (i + 1) * w))
+        if len(idx_L) > 0:
             L.append(np.mean(left_d[idx_L]))
-        if len(idx_R)> 0:
+        if len(idx_R) > 0:
             R.append(np.mean(right_d[idx_R]))
-    
-    metric = 1 - (np.mean(L + R)/np.sqrt(2))
+
+    metric = 1 - (np.mean(L + R) / np.sqrt(2))
     best_thresholds = thresholds[np.argmin(distances)]
-    
-    return metric , best_thresholds
+
+    return metric, best_thresholds
--- nested_cross_val/tools/feature_importances.py
+++ nested_cross_val/tools/feature_importances.py
@@ -1,102 +1,106 @@
+import warnings
+
+import matplotlib.axes
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import warnings
-from sklearn.cluster import AgglomerativeClustering
-from sklearn import manifold
 import umap
-import matplotlib.pyplot as plt
-import matplotlib.axes
+from sklearn import manifold
+from sklearn.cluster import AgglomerativeClustering
 
 
-def collect_importances(ncv , importances , feature_names = None):
+def collect_importances(ncv, estimator, importances, feature_names=None):
     """ Collect the feature importances through the outer folds of a fitted NestedCV object.
-    
+
     Parameters
     ----------
     ncv : NestedCV estimator
 
     importances : str or callable
         Method to obtain the feature importances (see get_attributes() method of NestedCV).
-        
+
         ex : importances = "coef_" (for linear models)
-        
+
     feature_names : list of strings, optionnal
         The default is None.
 
     Returns
     -------
     Dataframe, shape (n_features , n_outer_folds)
-    
+
     Note
     ------
-    ncv.get_attributes(importances) should return an array of shape (n_features, ). In particular, for estimators which integrate a feature 
+    ncv.get_attributes(importances) should return an array of shape (n_features, ). In particular, for estimators which integrate a feature
     selection step as pre-processing, features which were discarded should be re-integrated in the feature importances array (ex : using NaN).
 
     """
     temp = {}
     i = 0
-    for imp in ncv.get_attributes(attribute = importances):
-        temp['out fold ' + str(i)] = pd.Series(imp , index = feature_names)
-        i+=1
+    for imp in ncv.get_attributes(estimator=estimator, attribute=importances):
+        temp['out fold ' + str(i)] = pd.Series(imp, index=feature_names)
+        i += 1
     return pd.DataFrame(temp)
 
-def collect_meta_importances(ncv , importances , components ,  projections = None , full_components = None , full_projections = None 
-                             , clustering_space = "features" , consensus = "centrotypes" ,  plot = False , plot_method = "mds" , ax = None):
+
+def collect_meta_importances(ncv, estimator, importances, components, projections=None, full_components=None,
+                             full_projections=None
+                             , clustering_space="features", consensus="centrotypes", plot=False, plot_method="mds",
+                             ax=None):
     """ Collect and cluster the components obtained from the decomposition (ex : PCA) of the trainign data for each outer fold.
-    
+
     Parameters
     ----------
     ncv : NestedCV estimator
-        
+
     importances : str or callable
         Method to obtain the feature importances (see get_attributes() method of NestedCV).
-        
+
         ex : importances = importances = "coef_" (for linear models)
-        
+
     components : str or callable
         Method to obtain the components (see get_attributes() method of NestedCV).
-        
+
         ex : components = (lambda pipeline : pipeline['PCA'].components_)
-        
+
     projections : str or callable, optional
         Method to obtain the projection of the full data_set in the component space (see get_attributes() method of NestedCV).
         It should return an array of shape (n_samples , n_components).
         The default is None.
-        
+
         ex : projections = (lambda pipeline : pipeline['PCA'].transform(ncv.X_)). The default is None.
-        
+
     full_components : array, shape (n_components, n_features), optional
         Components coming from the decomposition of the full data set ncv.X_. The default is None.
-        
+
     full_projections : array, shape (n_samples , n_components), optional
         Projections of the full data set ncv.X_. The default is None.
-                
+
     clustering_space : str {"features" , "samples"}, optional
         The default is "features".
-        
+
         - "features" : each component will be represented by a vector (n_features, ) and the distances between these vectors
           will be used to cluster the components.
         - "Samples" : each component will be represented by a vector (n_samples, ) and the distances between these vectors
           will be used to cluster the components.
-          
+
         If clustering_space = "samples", the value of the projections parameter should not be None !
-        
+
     consensus : str {"centrotypes" , "full"}, optional
         Strategy to characterize each cluster. The default is "centrotypes".
-        
+
         - "centrotypes" :  each cluster will be characterized by its centrotype
         - "full" : each cluster will be characterized by the component coming from the decomposition of the full data set which
           belongs to this cluster.
-         
+
         If consensus = "full", the value of the full_components parameter should not be None. Besides, if clustering_space = "samples", the
         value of the full_projections parameter should also not be None !
-          
+
     plot : bool, optional
         If True, plot the projection of all the components in 2D. The default is False.
-        
+
     plot_method : str {"mds" , "tsne" , "umap"}, optional
         See _projection. The default is "mds".
-        
+
     ax : matplotlib.axes, optional
         The default is None.
 
@@ -104,98 +108,99 @@ def collect_meta_importances(ncv , importances , components ,  projections = Non
     -------
     df : Dataframe, shape (n_clusters , n_outer_folds)
         Feature importances for each outer fold of a NestedCV object.
-        
+
     stabilities : Series, shape (n_clusters)
         Stability indexes of the clusters.
-        
+
     Consensus : Dataframe, shape (n_clusters , n_features)
         Consensus components that characterize/represent the different clusters.
-        
+
     Note
     ------
-    
+
     Please note that this function is quite rigid and difficult to manipulate. We will try to improve it in the future !
 
     """
     # Collect components/meta-features accross the folds
-    L = [x for x in ncv.get_attributes(attribute = components)]
+    L = [x for x in ncv.get_attributes(estimator=estimator, attribute=components)]
     if full_components is not None:
-        L.append(full_components)    
-    C = np.vstack(L) 
+        L.append(full_components)
+    C = np.vstack(L)
     n_components = L[0].shape[0]
-    
+
     # Compute similarities between meta-features (either using the feature space or the "sample" space)
-    if clustering_space == "features" :
-        Sim = np.abs(np.corrcoef(x=C, rowvar=True))      
+    if clustering_space == "features":
+        Sim = np.abs(np.corrcoef(x=C, rowvar=True))
     elif clustering_space == "samples":
-        L = [x.T for x in ncv.get_attributes(attribute = projections)]
-        if full_projections is not None :
+        L = [x.T for x in ncv.get_attributes(estimator=estimator, attribute=projections)]
+        if full_projections is not None:
             L.append(full_projections.T)
         Sim = np.abs(np.corrcoef(x=np.vstack(L), rowvar=True))
-    
-    clustering = AgglomerativeClustering(n_clusters = n_components , affinity = "precomputed"  , linkage = 'average').fit(1 - Sim)      
-    
+
+    clustering = AgglomerativeClustering(n_clusters=n_components, affinity="precomputed", linkage='average').fit(
+        1 - Sim)
+
     # Associate each cluster with its feature importance for each fold
-    temp = {} 
-    i = 0       
-    for imp in ncv.get_attributes(attribute = importances):
-        index = ['cluster ' + str(j) for j in clustering.labels_[i*n_components : (i + 1)*n_components ]]
-        s = pd.Series(imp , index = index)
+    temp = {}
+    i = 0
+    for imp in ncv.get_attributes(estimator=estimator, attribute=importances):
+        index = ['cluster ' + str(j) for j in clustering.labels_[i * n_components: (i + 1) * n_components]]
+        s = pd.Series(imp, index=index)
         temp['out fold ' + str(i)] = s.groupby(s.index).mean()
-        i +=1
+        i += 1
     df = pd.DataFrame(temp)
-    
+
     # Compute the stability index of each cluster
     stabilities = np.zeros(n_components)
     for i in range(n_components):
-        cluster_labels = list(np.argwhere(clustering.labels_ == i ).flatten())
-        stabilities[i] = _stability_index(Sim , cluster_labels)
-    stabilities = pd.Series(stabilities , index = ['cluster ' + str(i) for i in range(n_components)])
-    
+        cluster_labels = list(np.argwhere(clustering.labels_ == i).flatten())
+        stabilities[i] = _stability_index(Sim, cluster_labels)
+    stabilities = pd.Series(stabilities, index=['cluster ' + str(i) for i in range(n_components)])
+
     # Compute a consensus meta-feature for each cluster
-    if consensus == "centrotypes" :
-        
-        Consensus = np.zeros((n_components , C.shape[1]))
+    if consensus == "centrotypes":
+
+        Consensus = np.zeros((n_components, C.shape[1]))
         for i in range(n_components):
             cluster_labels = list(np.argwhere(clustering.labels_ == i).flatten())
-            Consensus[i , :] = _centrotype(C , Sim , cluster_labels)
-        Consensus = pd.DataFrame(Consensus , index = ['cluster ' + str(i) for i in range(n_components)])  
-        
-    elif consensus == "full" :
-        
-        Consensus = pd.DataFrame(full_components , index = ['cluster ' + str(j) for j in clustering.labels_[- full_components.shape[0] :]])       
-    
-    # Plot a 2D projection of all the components
-    if plot :
+            Consensus[i, :] = _centrotype(C, Sim, cluster_labels)
+        Consensus = pd.DataFrame(Consensus, index=['cluster ' + str(i) for i in range(n_components)])
+
+    elif consensus == "full":
+
+        Consensus = pd.DataFrame(full_components,
+                                 index=['cluster ' + str(j) for j in clustering.labels_[- full_components.shape[0]:]])
+
+        # Plot a 2D projection of all the components
+    if plot:
         if (full_components is not None) or (full_projections is not None):
-            _projection(Sim , clustering.labels_ , method = plot_method
-                        ,specific_idx = np.arange(Sim.shape[0] , dtype = int)[- full_components.shape[0] :]  , ax = ax)
-        else :
-            _projection(Sim , clustering.labels_ , method = plot_method , ax = ax)
-            
-    return df , stabilities , Consensus
+            _projection(Sim, clustering.labels_, method=plot_method
+                        , specific_idx=np.arange(Sim.shape[0], dtype=int)[- full_components.shape[0]:], ax=ax)
+        else:
+            _projection(Sim, clustering.labels_, method=plot_method, ax=ax)
+
+    return df, stabilities, Consensus
 
 
-
-def _projection(Sim , cluster_labels , method = "mds" , specific_idx = None , ax = None):
+def _projection(Sim, cluster_labels, method="mds", specific_idx=None, ax=None):
     """ Project all the components in 2D using their similarity matrix.
-    
+
     Parameters
     ----------
     Sim : 2D array, shape (n_components , n_components)
-        Similarity matrix for the components 
-        
+        Similarity matrix for the components
+
     cluster_labels : ndarray of shape (n_components)
         cluster labels for each point
-        
+
     method : string, optional
         Name of the dimensionality reduction method (e.g "tsne" , "mds" or "umap")
         The default is "umap".
-    
+
     specific_idx : array-like, optional
-        Define specific components that will be represented with a red star on the 2D projection plot. 
+        Define specific components that will be represented with a red star on the 2D projection plot.
         The default is None.
-        
+
     ax : matplotlib.axes, optional
         The default is None.
 
@@ -204,39 +209,41 @@ def _projection(Sim , cluster_labels , method = "mds" , specific_idx = None , ax
     None.
 
     """
-    if ax is None : 
-        fig , ax = plt.subplots(figsize = (10 , 6))
-    elif not isinstance(ax , matplotlib.axes.Axes) :
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(10, 6))
+    elif not isinstance(ax, matplotlib.axes.Axes):
         warnings.warn("ax should be a matplotlib.axes.Axes object. It was redefined by default.")
-        fig , ax = plt.subplots(figsize = (10 , 6))
-        
+        fig, ax = plt.subplots(figsize=(10, 6))
+
     if method == "tsne":
-        embedding = manifold.TSNE(n_components = 2 , metric = "precomputed")
-    elif method == "mds" :
-        embedding = manifold.MDS(n_components = 2, dissimilarity = "precomputed" , n_jobs = -1)
-    elif method == "umap" : 
-        embedding = umap.UMAP(n_components = 2 , metric = "precomputed" )       
-        
+        embedding = manifold.TSNE(n_components=2, metric="precomputed")
+    elif method == "mds":
+        embedding = manifold.MDS(n_components=2, dissimilarity="precomputed", n_jobs=-1)
+    elif method == "umap":
+        embedding = umap.UMAP(n_components=2, metric="precomputed")
+
     P = embedding.fit_transform(np.sqrt(1 - Sim))
 
-    if specific_idx is not None :
+    if specific_idx is not None:
         other_idx = list(set(range(P.shape[0])) - set(specific_idx))
-        ax.scatter(P[other_idx  , 0] , P[other_idx  , 1] , c=cluster_labels[other_idx] , cmap = 'viridis' , label = "ncv components")
-        ax.scatter(P[specific_idx , 0] , P[specific_idx , 1] , marker = "*" , s = 100 , c = "red" , label = "full components")
+        ax.scatter(P[other_idx, 0], P[other_idx, 1], c=cluster_labels[other_idx], cmap='viridis',
+                   label="ncv components")
+        ax.scatter(P[specific_idx, 0], P[specific_idx, 1], marker="*", s=100, c="red", label="full components")
         ax.legend()
-    else : 
-        ax.scatter(P[: , 0] , P[: , 1] , c= cluster_labels , cmap = 'viridis')       
-    ax.set_title("2D embedding of the clusters of components (" + method +")", fontsize = 14)
+    else:
+        ax.scatter(P[:, 0], P[:, 1], c=cluster_labels, cmap='viridis')
+    ax.set_title("2D embedding of the clusters of components (" + method + ")", fontsize=14)
     return
 
-def _stability_index(Sim , cluster_labels):
+
+def _stability_index(Sim, cluster_labels):
     """Compute the stability index for the cluster of components defined by cluster_labels.
-        
+
     Parameters
     ----------
     Sim : 2D array, shape (n_components , n_components)
         Similarity matrix
-        
+
     cluster_labels : list of integers
         Indexes of the cluster of components (ex:[0 , 1 , 7] refers to the rows 0, 1 and 7 of X)
 
@@ -246,29 +253,30 @@ def _stability_index(Sim , cluster_labels):
         Stability index for the cluster of components defined by cluster_labels
 
     """
-    temp = Sim[np.ix_(cluster_labels , cluster_labels)]
+    temp = Sim[np.ix_(cluster_labels, cluster_labels)]
     ex_cluster = list(set(range(Sim.shape[1])) - set(cluster_labels))
-    
-    #aics = average intra-cluster similarities
-    aics = (1/len(cluster_labels)**2)*np.sum(temp)  
-    
-    #aecs = average extra-cluster similarities
-    aecs = (1/(len(ex_cluster)*len(cluster_labels)))*np.sum(Sim[np.ix_(cluster_labels , ex_cluster)]) 
-    
+
+    # aics = average intra-cluster similarities
+    aics = (1 / len(cluster_labels) ** 2) * np.sum(temp)
+
+    # aecs = average extra-cluster similarities
+    aecs = (1 / (len(ex_cluster) * len(cluster_labels))) * np.sum(Sim[np.ix_(cluster_labels, ex_cluster)])
+
     return aics - aecs
 
-def _centrotype(X , Sim , cluster_labels):
+
+def _centrotype(X, Sim, cluster_labels):
     """Compute the centrotype of the cluster of components defined by cluster_labels
-    
+
        centrotype : component of the cluster which is the most similar to the other components
                    of the cluster
     Parameters
     ----------
     X : 2D array, shape (n_components , n_observations)
-        
+
     Sim : 2D array, shape (n_components , n_components)
         Similarity matrix for the components (i.e rows of X)
-        
+
     cluster_labels : list of integers
         Indexes of the cluster of components (ex:[0 , 1 , 7] refers to the rows 0, 1 and 7 of X)
 
@@ -278,5 +286,5 @@ def _centrotype(X , Sim , cluster_labels):
         Centrotype of the cluster of components defined by cluster_labels
 
     """
-    temp = np.argmax(np.sum(Sim[np.ix_(cluster_labels , cluster_labels)] , axis=0))
-    return X[cluster_labels[temp] , :]
+    temp = np.argmax(np.sum(Sim[np.ix_(cluster_labels, cluster_labels)], axis=0))
+    return X[cluster_labels[temp], :]
